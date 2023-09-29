66ee787bd625e7ede988d3b7d9a423ae5734d783
Nelkeasha <igihozonelly3@gmail.com>
2023-09-29 10:42:15 -0700

    4-add.c

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -8,5 +8,6 @@ int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[] __attribute__((unused)));
 int main(int argc, char *argv[]);
 int _atoi(char *s);
+int _atoi(char *s);
 
 #endif /* MAIN_H */
