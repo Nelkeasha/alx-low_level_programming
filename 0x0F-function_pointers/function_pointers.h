d07dc125c758ad3e117fe79b5fe7fb58176aa21e
Nelkeasha <igihozonelly3@gmail.com>
2023-10-13 11:45:25 -0700

    3-main.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,5 +7,9 @@ void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+int (*f)(int a, int b);
+int (*get_op_func(char *s))(int, int);
+int op_sub(int a, int b);
+int main(int argc, char *argv[]);
 
 #endif /* MAIN_H */
