041bb62fdf675c339d33ad241b0e3945549668c8
Nelkeasha <igihozonelly3@gmail.com>
2023-10-31 04:18:41 -0700

    Delete unnecessary files and directories

--- README.md
+++
@@ -1,2 +0,0 @@
-this is readme for this project
-an update
--- main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
-ssize_t read_textfile(const char *filename, size_t letters);
-int _putchar(char c);
-
-#endif /* MAIN_H */
